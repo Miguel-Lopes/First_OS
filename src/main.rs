445e3e849cd62ffa7bd6a6f3729b1d5dbca36f31
Miguel Lopes <Miguelfarialopes2000@gmail.com>
2024-08-30 16:22:28 +0200

    Minimalistic Kernel
    Skipped building the bootloader, used bootimage instead (https://github.com/rust-osdev/bootimage) but a native bootloader is planned.
    
    Currently using BIOS instead of UEFI but UEFI is planned to be used in the future!
    
    In its current form if executed the OS prints "Hello World!" to the VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-#![no_std]
-#![no_main]
+#![no_std] // Dont connect to the Rust standard library since we trying to make our own
+#![no_main] // disable all entry points at the Rust-level 
 
 use core::panic::PanicInfo;
 
-//Function called when Panic is triggered
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // don't mangle the function name so i dont create a 30 digit monstrosity
+pub extern "C" fn _start() -> ! {
+    // this function will be the entry point, since the linker will try to find a function
+    // named `_start` by default
+
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+/// This function is called on panic (in case something bad happens).
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
