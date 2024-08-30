eb8ca84a1601a58225aab6a80eae454930a55e06
Miguel Lopes <Miguelfarialopes2000@gmail.com>
2024-08-30 20:07:56 +0200

    VGA Text Buffer
    Added basic support for the VGA text buffer.
    Added my own print function
    Now errors messages get printed in the BIOS screen
    
    TO DO
    Add comments in vga_buffer functions.

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,16 @@ use core::panic::PanicInfo;
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the function name so i dont create a 30 digit monstrosity
-pub extern "C" fn _start() -> ! {
-    // this function will be the entry point, since the linker will try to find a function
-    // named `_start` by default
-
-    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
 
     loop {}
 }
 
+
 /// This function is called on panic (in case something bad happens).
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
