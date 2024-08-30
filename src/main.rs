18b78ad3ca7b55736bd44a6ce1006ce61bd59660
Miguel Lopes <Miguelfarialopes2000@gmail.com>
2024-08-30 11:41:06 +0200

    Freestanding Binary
    At this point i made a freestanding binary that doesnt require the Rust standard library to compile.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-
 #![no_std]
+#![no_main]
 
-fn main() {
+use core::panic::PanicInfo;
 
+//Function called when Panic is triggered
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
